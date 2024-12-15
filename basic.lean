e8a714803863ae12bd64d05c109c9c70eca3a7ea
Karthik Nishanth <nishanthkarthik@live.com>
2024-12-15 14:25:32 -0800

    wip

--- basic.lean
+++ basic.lean
@@ -1,3 +1,5 @@
+import Mathlib.Algebra.Group.Nat.Even
+
 variable (a b : Prop)
 
 theorem p : a ∨ ¬a := Classical.em a
@@ -28,3 +30,31 @@ theorem id_exists (i : Int) : ∃(j : Int), j = i := by
   have h : (i = i) := rfl
   constructor
   exact h
+
+structure Result where
+  original: Nat
+  rounded_up: Nat
+  inv_a: original <= rounded_up
+  inv_b: rounded_up - original <= 1
+  inv_c: rounded_up % 2 = 0
+
+def round_up_even (n : Nat) : Result :=
+  let r := if n % 2 = 0 then n else n + 1
+
+  have inv_a : n <= r := by
+    unfold r; split
+    all_goals simp [Nat.le_refl, Nat.le_add_right]
+
+  have inv_b : r - n <= 1 := by
+    unfold r; split
+    . rw [Nat.sub_self]
+      simp [Nat.zero_le]
+    . rw [Nat.add_sub_cancel_left n 1]
+
+  have inv_c : r % 2 = 0 := by
+    unfold r; split <;> rename_i h
+    . exact h
+    . simp [Nat.succ_mod_two_eq_zero_iff]
+      exact Nat.mod_two_ne_zero.mp h
+
+  Result.mk n r inv_a inv_b inv_c
